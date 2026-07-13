64bb6b284ecbdb9c7d49cd3633c83b7a592a9f91
muhammedShifas <muhammedshifas73@gmail.com>
2026-07-13 13:16:33 +0530

    prompt practiced

--- basic.js
+++ basic.js
@@ -1,8 +1,15 @@
-let a= 10
-let b=20
-let temp=a
-a=b
-b=temp
+// let a= 10
+// let b=20
+// let temp=a
+// a=b
+// b=temp
 
 
-console.log(a,b)
+// console.log(a,b)
+
+let name = prompt("Enter your full name")
+let age = parseInt(prompt("Enter your age"))
+let email = prompt("Enter your email")
+console.log("Your name is ",name )
+console.log("Your age is ",age )
+console.log("Your email is ",email )
