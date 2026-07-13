53c556354abb6ae2bf70c21cdb593e1d6fb27598
muhammedShifas <muhammedshifas73@gmail.com>
2026-07-13 13:51:30 +0530

    3-number swapping practiced

--- basic.js
+++ basic.js
@@ -15,14 +15,35 @@
 // console.log("Your email is ",email )
 
 
-let a=parseInt(prompt("enter number-1: "))
-let b=parseInt(prompt("enter number-2: ")) 
+// let a=parseInt(prompt("enter number-1: "))
+// let b=parseInt(prompt("enter number-2: ")) 
 
-let add=(a+b)
-let sub=(a-b)
-let mul=(a*b)
-let div=(a/b)
-console.log("Addition result=",add)
-console.log("subtraction result=",sub)
-console.log("multiplication result=",mul)
-console.log("division result=",div)
+// let add=(a+b)
+// let sub=(a-b)
+// let mul=(a*b)
+// let div=(a/b)
+// console.log("Addition result=",add)
+// console.log("subtraction result=",sub)
+// console.log("multiplication result=",mul)
+// console.log("division result=",div)
+
+
+let A=parseInt(prompt("enter number-A: "))
+let B=parseInt(prompt("enter number-B: "))
+let C=parseInt(prompt("enter number-C: ")) 
+
+
+console.log("the number A is ",A)
+console.log("the number B is ",B)
+console.log("the number C is ",C)
+
+let temp=A
+A=C
+C=B
+B=temp
+
+
+alert("the value of A is "+A)
+alert("the value of B is "+B)
+alert("the value of C is "+C)
+
