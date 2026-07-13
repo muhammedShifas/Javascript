faa3facdaaf829efe141a015da2eceb15dc27dc7
muhammedShifas <muhammedshifas73@gmail.com>
2026-07-13 13:27:20 +0530

    add,sub,mul,div practiced by prompting

--- basic.js
+++ basic.js
@@ -7,9 +7,22 @@
 
 // console.log(a,b)
 
-let name = prompt("Enter your full name")
-let age = parseInt(prompt("Enter your age"))
-let email = prompt("Enter your email")
-console.log("Your name is ",name )
-console.log("Your age is ",age )
-console.log("Your email is ",email )
+// let name = prompt("Enter your full name")
+// let age = parseInt(prompt("Enter your age"))
+// let email = prompt("Enter your email")
+// console.log("Your name is ",name )
+// console.log("Your age is ",age )
+// console.log("Your email is ",email )
+
+
+let a=parseInt(prompt("enter number-1: "))
+let b=parseInt(prompt("enter number-2: ")) 
+
+let add=(a+b)
+let sub=(a-b)
+let mul=(a*b)
+let div=(a/b)
+console.log("Addition result=",add)
+console.log("subtraction result=",sub)
+console.log("multiplication result=",mul)
+console.log("division result=",div)
