a832100f7f625a13ddd51718f4a3d53d03148990
muhammedShifas <muhammedshifas73@gmail.com>
2026-07-13 22:58:21 +0530

    fuel cost calculation practiced

---
+++ cost.js
@@ -0,0 +1,10 @@
+let dis= parseInt(prompt("Enter the distance to travel: "))
+let mil= parseInt(prompt("Enter the mileage of your bike: "))
+
+temp= dis/mil 
+
+total= temp*100
+ 
+alert("Distance: "+dis)
+alert("Mileage: "+mil)
+alert("Total cost: ₹"+total)
