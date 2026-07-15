163a95382788c1157e51e9ab138355c7b1f6f1ff
muhammedShifas <muhammedshifas73@gmail.com>
2026-07-15 12:24:10 +0530

    elevator code added

---
+++ ele.js
@@ -0,0 +1,5 @@
+let total= parseInt(prompt("Enter the total no. of people: "))
+let trips= Math.floor(total/8)
+let rem= total%8
+alert("Full Elevator Trips: "+trips)
+alert("Remaining people: "+rem)
