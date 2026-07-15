cd0ca0899aa48cc1abe30270a35c3e3e11f11484
muhammedShifas <muhammedshifas73@gmail.com>
2026-07-15 15:41:44 +0530

    water tank code added

---
+++ water.js
@@ -0,0 +1,9 @@
+let tank= parseInt(prompt("Enter the tank capacity: "))
+let final= tank*0.80
+let rain= final-320
+let use= rain+180
+factory= 180
+let temp= use/3 
+let rain2= 320
+let temp2= temp*4
+alert("Tank capacity: "+tank+"\n"+"Initial water: "+temp2+"\n"+"Water evaporated: "+temp+"\n"+"factory usage: "+factory+"\n"+"Rain water added: "+rain2+"\n"+"Final water: "+final)
