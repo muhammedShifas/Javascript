2d224cee49d616a03fedffadb6dbe890811ecab5
muhammedShifas <muhammedshifas73@gmail.com>
2026-07-17 13:03:32 +0530

    if else code added

---
+++ ifelse.js
@@ -0,0 +1,11 @@
+let age= parseInt(prompt("Enter your age: "))
+
+if(age==18 || age==60){
+    alert("You are eligible to vote")
+}
+else if(age == 100){
+     alert("Century")
+}
+else{
+    alert("You are not eligible to vote")
+}
