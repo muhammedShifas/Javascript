1a82c65609653619230f1bf67615eee6f0783436
muhammedShifas <muhammedshifas73@gmail.com>
2026-07-21 11:38:15 +0530

    updated code

--- namesearch.js
+++ namesearch.js
@@ -1,17 +1,20 @@
-let numbers= [1,2,3,45,46,67,85,99]
+let numbers= [1,2,3,45,46,67,55,99]
 let num= parseInt(prompt("Enter the number to be searched: "))
 
+let found= false
 for(i=0;i<numbers.length;i++)
 {
-   if(numbers[i] == num){
-          alert("The number found: "+num+"\n"+"The position of the number is: "+i)
-        
+   if(numbers[i] == num)
+   {
+    found= true
+    if(found==true){
+    alert("Number found "+"\n"+"The position of the number is: "+i)
+    }
    }
- else if(i == numbers.length-1){
-   alert("Not found")
- }
 }
 
-
+if (found==false){
+  alert("Not found")
+}
 
 
