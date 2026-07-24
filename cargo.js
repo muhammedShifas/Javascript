b8456b1d17bc13b83afc42f5a488327cdefc1772
muhammedShifas <muhammedshifas73@gmail.com>
2026-07-24 14:03:15 +0530

    cargo code edited

--- cargo.js
+++ cargo.js
@@ -1,18 +1,15 @@
 let weights= [1200,200]
 let total= 0
-let skip= 0
-let box= 0
 let loaded=[]
 let Capacity= 1000
 let cannot= 0
 
 
-for(i=0;i<weights.length;i++)
+for(let i=0;i<weights.length;i++)
 {
    if(total+weights[i]<=Capacity){
     total= total+weights[i]
-    box= weights[i]
-    loaded.push(box)
+    loaded.push(weights[i])
    }
    else{
     cannot= weights[i]
@@ -30,5 +27,5 @@ console.log("Total weights: ",total);
 console.log("Boxes loaded: ",boxesloaded);
 console.log("Boxes remaining: ",rem);
 if(rem>0){
-console.log("Next box (",cannot+" )cannot be loaded because it exceeds the truck capacity");
+console.log("Next box ("+cannot+"kg) cannot be loaded because it exceeds the truck capacity");
 }
